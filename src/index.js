0b259c09b46aaa5a1da5b32b2e6ab755bd5743f2
DimaDK <DimaKrutDK@ya.ru>
2018-09-27 10:32:01 +0300

    implemented algorithm but I suppose I can improve it

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  for (const brackets of bracketsConfig) {
+    for (const brackets of bracketsConfig) {
+      for (const brackets of bracketsConfig) {
+        const [open, close] = brackets;
+        const searchBracketPair = open + close;
+        while (str.includes(searchBracketPair)) {
+          const openBracketPosition = str.indexOf(searchBracketPair);
+          str =
+            str.slice(0, openBracketPosition) +
+            str.slice(openBracketPosition + 2);
+        }
+      }
+    }
+  }
+  return str.length === 0;
+};
