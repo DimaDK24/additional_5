299ee7be81fc173a2c83bbc18dcc9230f5f82959
DimaDK <DimaKrutDK@ya.ru>
2018-09-27 10:49:29 +0300

    Yeah, better implemetation :)

--- src/index.js
+++ src/index.js
@@ -1,16 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  for (const brackets of bracketsConfig) {
-    for (const brackets of bracketsConfig) {
-      for (const brackets of bracketsConfig) {
-        const [open, close] = brackets;
-        const searchBracketPair = open + close;
-        while (str.includes(searchBracketPair)) {
-          const openBracketPosition = str.indexOf(searchBracketPair);
-          str =
-            str.slice(0, openBracketPosition) +
-            str.slice(openBracketPosition + 2);
-        }
-      }
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    const brackets = bracketsConfig[i];
+    const [open, close] = brackets;
+    const searchBracketPair = open + close;
+    while (str.includes(searchBracketPair)) {
+      const openBracketPosition = str.indexOf(searchBracketPair);
+      str =
+        str.slice(0, openBracketPosition) + str.slice(openBracketPosition + 2);
+      i = -1;
     }
   }
   return str.length === 0;
